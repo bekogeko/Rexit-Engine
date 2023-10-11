6e8f2cd307962bc190b443b9780b5a8ee8f8cd24
Bekir Gülestan <gulestanbekir@gmail.com>
2023-10-11 05:45:58 -0500

    Add First Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,42 @@
 
 #include "lib.h"
 
+auto vertexShaderSource = "#version 330 core\n"
+                          "layout (location = 0) in vec3 aPos;\n"
+                          "void main()\n"
+                          "{\n"
+                          "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                          "}\0";
+auto fragmentShaderSource = "#version 330 core\n"
+                            "out vec4 FragColor;\n"
+                            "void main()\n"
+                            "{\n"
+                            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                            "}\n\0";
+
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f, // left
+    0.5f, -0.5f, 0.0f,  // right
+    0.0f, 0.5f, 0.0f    // top
+};
+
+// VAO and VBO
+unsigned int VAO, VBO;
+unsigned int program, vertexShader, fragmentShader;
+
 int main()
 {
+  std::cout << "Hello World" << std::endl;
+
   if (!glfwInit())
   {
     std::cout << "Error: GLFW could not be initialized" << std::endl;
     return -1;
   }
+  // Required for Mac OS X (At least on my machine)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (window == NULL)
@@ -30,8 +59,50 @@ int main()
     return -1;
   }
 
+  // Shader program to
+  // 1. Compile vertex and fragment shaders
+  // 2. Link compiled shaders
+  // 3. Delete compiled shaders
+  program = glCreateProgram();
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+  glCompileShader(vertexShader);
+  glCompileShader(fragmentShader);
+
+  glAttachShader(program, vertexShader);
+  glAttachShader(program, fragmentShader);
+
+  glLinkProgram(program);
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  // Bind VAO and VBO
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+
+  glEnableVertexAttribArray(0);
+
   while (!glfwWindowShouldClose(window))
   {
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // // render the triangle
+    glUseProgram(program);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
 
